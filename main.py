58d78d47d4fc0a5ce0b05c5005086ef9faf54198
Akramjon <akramjonakbaraliev2010@gmail.com>
2025-07-18 14:54:18 +0500

    main.py is added

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 from telebot import TeleBot
+from telebot.types import Message
 
-TOKEN = ""
+TOKEN = "7373419973:AAEYMy7UbcQF0fMi_WBjdNypLUIye3iZD9A"
 
+bot = TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message: Message):
+    chat_id = message.chat.id
+    bot.send_message(chat_id, f"Hello {message.from_user.first_name}!")
+
+
+
+
+if __name__ == '__main__':
+    bot.infinity_polling()
